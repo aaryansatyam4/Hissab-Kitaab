6574d3702559418f2f154643482999cf178e0c20
aaryansatyam4 <aaryansatyam4@gmail.com>
2024-01-01 11:49:08 +0530

    Initial Commit

---
+++ Hissab Kitaab/Hissab_KitaabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hissab_KitaabApp.swift
+//  Hissab Kitaab
+//
+//  Created by aryan on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Hissab_KitaabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
